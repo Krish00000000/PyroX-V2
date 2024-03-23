491ee58ae09f55b4969a8a6df6fd8c6cd0c978c9
Krish <144118439+Krish00000000@users.noreply.github.com>
2024-03-23 17:26:05 +0530

    Update mass_action.py

--- Barath/plugins/mass_action.py
+++ Barath/plugins/mass_action.py
@@ -1,1 +1,18 @@
+from Barath import barath as app
+from pyrogram.types import Message
+from pyrogram.enums import ChatMemberStatus
+import config
 
+@app.on_message(filters.command("banall", prefixes=config.HANDLER) & filters.me)
+async def ban_all_members(_, message: Message):
+    x = await app.send_message(message.chat.id, "🖤 Black Magic Started. 🪄")
+    try:
+        count = 0
+        async for user in app.iter_chat_members(message.chat.id):
+            if user.status == ChatMemberStatus.MEMBER:
+                await app.kick_chat_member(message.chat.id, user.user.id)
+                count += 1
+        await x.edit_text(f"Banned 🚫 {count} Members.")
+    except Exception as e:
+        await x.edit_text("Something Went Wrong. Contact Developers loda le le bsdk")
+      
