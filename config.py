615d70dd642f86b191dad28c0f384ee435ec2943
Krish <144118439+Krish00000000@users.noreply.github.com>
2024-03-24 19:07:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 OWNER_ID = os.getenv("6217632586",1238234357)
 HANDLER = ["~", ".","!","?","@","$"]
-BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
+BARATH = "https://telegra.ph//file/af8778df92724ee72f259.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
 SOURCE = "https://github.com/SIAmKira/PyroX-V2"
 LOG_CHAT = -1002119409366
