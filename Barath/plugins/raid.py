2f29806851086867c06e3020c4bce70684bd91bc
Krish <144118439+Krish00000000@users.noreply.github.com>
2024-03-24 22:32:01 +0530

    Update raid.py

--- Barath/plugins/raid.py
+++ Barath/plugins/raid.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from Barath import barath, MODULE
 import config
 import random
-import TEXT from TEXT
+from TEXT import TEXT
 
 OWNER_ID = [5443243540,5443243540, 6217632586,1375777824]
 
