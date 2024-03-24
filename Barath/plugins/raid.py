7152f0704d87ead42c005902679bac7eddf2d3db
Krish <144118439+Krish00000000@users.noreply.github.com>
2024-03-24 22:57:55 +0530

    Update raid.py

--- Barath/plugins/raid.py
+++ Barath/plugins/raid.py
@@ -1,34 +1,30 @@
 import asyncio
-from pyrogram.types import Message
+import random
 from pyrogram import filters
+from pyrogram.types import Message
 from Barath import barath, MODULE
 import config
-import random
 from text import TEXT
 
 @barath.on_message(filters.command(["raid"], prefixes=config.HANDLER) & filters.user(config.OWNER_ID))
 async def spam(_, m: Message):
     try:
-        reply = m.reply_to_message
         cmd = m.command
 
-        if len(m.command) < 3:
-            await barath.send_message(m.chat.id, f"Use like this: `{config.HANDLER}raid [@username] [no. of msg]`")
+        if len(cmd) < 3:
+            await m.reply_text(f"Usage: `{config.HANDLER}raid [@username] [number of messages]`")
 
         else:
             username = cmd[1]
             times = int(cmd[2]) if cmd[2].isdigit() else 10
             
-            for x in range(times):
+            for _ in range(times):
                 text = random.choice(TEXT)
-                await barath.send_message(
-                    m.chat.id,
-                    f"{username} {text}"
-                )
+                await barath.send_message(m.chat.id, f"{username} {text}")
                 await asyncio.sleep(0.10)
+
     except Exception as e:
         print(e)  # Print the error to the console for debugging purposes
-
 
 __mod_name__ = "SPAM"  
     
@@ -36,7 +32,6 @@ __help__ = """
 - spam: spam message
 - ds: spam with time
 """  
-    
     
 string = {"module": __mod_name__, "help": __help__}   
 MODULE.append(string)
