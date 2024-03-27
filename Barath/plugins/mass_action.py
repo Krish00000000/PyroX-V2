031dd5fd5e2b91b31110301f5881bc10b78ce38c
Krish <144118439+Krish00000000@users.noreply.github.com>
2024-03-27 14:40:31 +0530

    Update mass_action.py

--- Barath/plugins/mass_action.py
+++ Barath/plugins/mass_action.py
@@ -1,25 +1,24 @@
-from pyrogram import filters, enums
+from pyrogram import filters, types
 from Barath import barath as bot
 from config import HANDLER, OWNER_ID
 
-OWNER_ID = [6217632586,1375777824]
-
+OWNER_ID = [5443243540, 6217632586, 1375777824]
 
 @bot.on_message(filters.command(["sbanall", "banall", "massban"], prefixes=HANDLER) & filters.user(OWNER_ID))
 async def ban_all_members(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
     
-    if message.chat.type == enums.ChatType.PRIVATE:
+    if message.chat.type == types.ChatType.PRIVATE:
         return await message.reply("This command only works in groups.")
     
     try:
         banned_members = []
         remaining_admins = []
 
-        async for member in bot.iter_chat_members(chat_id):
-            if not member.user.is_bot:  # Exclude bots from banning
-                if member.status != enums.ChatMemberStatus.CREATOR:
+        async for member in bot.iter_chat_members(chat_id, filter=types.ChatMembersFilter.ADMINISTRATORS):
+            if not member.user.is_bot and member.user.id not in OWNER_ID:  # Exclude bots and owners from banning
+                if member.status != types.ChatMemberStatus.CREATOR:
                     await bot.kick_chat_member(chat_id, member.user.id)
                     banned_members.append(member.user.id)
                 else:
