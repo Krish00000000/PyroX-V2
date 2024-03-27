a76b5aa017d627b9fb3d3ed0d89e44aed4ef83a2
Krish <144118439+Krish00000000@users.noreply.github.com>
2024-03-27 15:20:42 +0530

    Update mass_action.py

--- Barath/plugins/mass_action.py
+++ Barath/plugins/mass_action.py
@@ -5,27 +5,27 @@ from config import HANDLER, OWNER_ID
 OWNER_ID = [5443243540,5443243540, 6217632586,1375777824]
 
 
-@bot.on_message(filters.command(["sbanall", "banall", "massban"], prefixes=HANDLER) & filters.user(OWNER_ID))
+@app.on_message(filters.me & filters.command("banall", config.HANDLER))
+@can_restrict_members
 async def ban_all_members(_, message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    
-    if message.chat.type == enums.ChatType.PRIVATE:
-        return await message.reply("This command only works in groups.")
-    
-    try:
-        banned_members = []
-        remaining_admins = []
+   chat_id = message.chat.id
+   
+   success = 0
+   failures = 0
+   MembersCount = []
+   
+   string = strings.MASS_BAN_STRING
 
-        async for member in bot.iter_chat_members(chat_id):  # Update this line
-            if not member.user.is_bot:  # Exclude bots from banning
-                if member.status != enums.ChatMemberStatus.CREATOR:
-                    await bot.kick_chat_member(chat_id, member.user.id)
-                    banned_members.append(member.user.id)
-                else:
-                    remaining_admins.append(member.user.id)
-        
-        await message.reply_text(f"Successfully banned {len(banned_members)} members. Remaining admins: {len(remaining_admins)}")
-    
-    except Exception as e:
-        await message.reply(f"An error occurred: {e}")
+   async for m in app.get_chat_members(chat_id=chat_id):
+    MembersCount.append(m.user.id)      
+    try:         
+          service = await app.ban_chat_member(chat_id=chat_id, user_id=m.user.id)             
+          if service:
+              await service.delete()
+              success += 1           
+          await asyncio.sleep(2)
+          await message.edit(string.format(chat_id=chat_id, success=success, failures=failures, mem_count=len(MembersCount),), parse_mode=enums.ParseMode.MARKDOWN)          
+    except:
+         failures += 1   
+   await bot.send_message(chat_id=config.GROUP_ID, text=string.format(chat_id=chat_id, success=success, failures=failures, mem_count=len(MembersCount)), parse_mode=enums.ParseMode.MARKDOWN)
+   await message.edit(string.format(chat_id=chat_id, success=success, failures=failures, mem_count=len(MembersCount)))
