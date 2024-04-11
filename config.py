bef9d15e6021ecc34fb99a2d111975a57e2c0de6
Krish <144118439+Krish00000000@users.noreply.github.com>
2024-04-11 22:06:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-OWNER_ID = os.getenv("6217632586")
+OWNER_ID = os.getenv("5443243540,5443243540, 6217632586,1375777824")
 HANDLER = ["~", ".","!","?","@","$"]
 BARATH = "https://telegra.ph//file/af8778df92724ee72f259.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
