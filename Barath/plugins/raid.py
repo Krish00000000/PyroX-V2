9455806c94a16ca15f620d9884bfad0a3215863d
Krish <144118439+Krish00000000@users.noreply.github.com>
2024-04-18 23:07:42 +0530

    Update raid.py

--- Barath/plugins/raid.py
+++ Barath/plugins/raid.py
@@ -8,7 +8,47 @@ import random
 # Devs ya authorized users ki IDs dalen
 DEVS = [5443243540, 6217632586, 1375777824]
 
-TEXT = ["Bhosdike madarchod bhen k lode maachar ki jhaat gandu maa ki chut", "teri tatto ke soudagaR  ",
+TEXT = ["Teri ma ka bhosda sale maderchod ki aulad 🤬",
+
+"GAND KII DHAAR BHOSDIKE FATEE HUE CONDOM KI NAAJAIS PAIDAISH",
+
+"Teri maa ki choot gand kay tatto teri maa ka bhosda karke uski gaand mai ping pong kar dunga",
+
+"Phatele Nirodh Ke Natije! 😂😂",
+
+"Teri Gaand Mein Kutte Ka Lund 🖕 Teri Jhaatein Kaat Kar Tere Mooh Par Laga Kar Unki French Beard Bana Doonga!",
+
+"chut kay baal nipple ki dhaar teri gaand mai Road roller de dunga 🖕🤬",
+
+"Chullu Bhar Muth Mein Doob Mar Kaali Chut Ke Safed Jhaat",
+
+"chutiye behenchod lauda madarchod gaandu bhosadikey",
+
+"Gote Kitne Bhi Badey Ho, Lund Ke Niche Hi Rehtein Hain",
+
+"Chipkali ki bhigi chut Choot kay baal Chipkali ke jhaat ke paseene",
+
+"madar chod bhosdke esa lagta h apne hii taaate kaat ke chipka diya apni shakal dekh lodee jese shakal aur gand me h aakal",
+
+"Teri ma ki gand me hathi ka lund dalke asa chodunga Na Bacha hojayega Johny sins ,ke lund se chudwaungu bhosdike",
+
+"14 baap ki Najais olad randi kay beez chinnale",
+
+"GAND MAI VIMAL KI GOLI BNA KAR DE DUNGA BHENCHO TERI GAAND MAI RAILWAY STATION KA FATAK DE DUNGA 😂😂🤬🖕",
+
+"teri maa k bhosde mai MDH CHANA MASALA daal k tere baap ko vo spicy bhosda khila dunga 🥵🤮",
+
+"SALA TARI BHAN KO ROAD PA LAJA KA KA NANGA KAR KA BAACHO SA CHUD VAU",
+
+"teri ma Randi tera baap hizda kaali gaand kay Khade baal jhaatu Randi kay chodu",
+
+"TERA BAAP JOHNY SINS CIRCUS KAY BHOSDE JOKER KI CHIDAAS 14 LUND KI DHAAR TERI MUMMY KI CHUT MAI 200 INCH KA LUND",
+
+"maa k lode tere jese randi k baccho ko bachpan mai maar dena chiye",
+
+"madarchod chutmarke teri tatti jesi shakl pe pad dunga bhen k lode chutiye",
+
+"Bhenchod baap se panga matt le Warna maa chodh di Jayegi 🤬","Bhosdike madarchod bhen k lode maachar ki jhaat gandu maa ki chut", "teri tatto ke soudagaR  ",
 "hijde ki aulad",
 "Kutte ke poot, teri maa ki choot Lavde ke bal Lund Chus Lundoos",
 "Lund Ke Pasine",
